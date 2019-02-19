f0c97c48ebbfda665a496a2029d29bfa44fc5ebf
Joao Ubaldo Xavier Reis <joao.reis21@gmail.com>
2019-02-18 22:19:54 -0300

    Initial parsing

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,72 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+
 public class JsonUtils {
 
+    private static final String TAG = JsonUtils.class.getSimpleName();
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        JSONObject sandwichData = null;
+
+        String mainName = "";
+        List<String> alsoKnownAs = null;
+        String placeOfOrigin = "";
+        String description = "";
+        String image = "";
+        List<String> ingredients = null;
+
+        try {
+            sandwichData = new JSONObject(json);
+            Iterator<String> keys = sandwichData.keys();
+
+            while (keys.hasNext()) {
+                String key = keys.next();
+
+                switch (key) {
+                    case "name":
+                        JSONObject nameJsonObject = sandwichData.getJSONObject(key);
+                        JSONArray akaJsonArray = nameJsonObject.getJSONArray("alsoKnownAs");
+
+                        mainName = nameJsonObject.getString("mainName");
+                        break;
+
+                    case "placeOfOrigin":
+                        placeOfOrigin = sandwichData.getString("placeOfOrigin");
+                        break;
+
+                    case "description":
+                        description = sandwichData.getString("description");
+                        break;
+
+                    case "image":
+                        image = sandwichData.getString("image");
+                        break;
+
+                    case "ingredients":
+                        JSONArray ingredientList = sandwichData.getJSONArray(key);
+                        break;
+                }
+
+                System.out.println(">>>>>>>   " + key);
+
+            }
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        return new Sandwich(mainName,alsoKnownAs,placeOfOrigin,description,image,ingredients);
     }
 }
